8d9d45f936b3c1bd1c47402a799c4af274394e1f
TEGAR_71105 <tegardito02@gmail.com>
2023-05-23 18:06:23 +0700

    update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $username = "root";
 $password = "pw";
-$hostname = "3.235.168.23";
+$hostname = "localhost";
 $dbname = "booku";
 
 $dbhandle = mysqli_connect($hostname, $username, $password, $dbname) or die(mysqli_error());
